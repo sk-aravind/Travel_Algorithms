527a2bfc2883a229c8671745371014e43a25f97d
sk-aravinda <sk.aravind.93@gmail.com>
2016-11-10 02:57:28 +0800

    Base Structure,UI elements and Object Classes

---
+++ app/src/main/java/com/skara/triggered/travelapp_triggered/RVAdapter.java
@@ -0,0 +1,63 @@
+package com.skara.triggered.travelapp_triggered;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Skara on 9/11/16.
+ */
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder>{
+//all the methods needs to be overwritten to prevent error
+
+    List<HomeScreen.Destination> dest_list;
+
+    RVAdapter(List<HomeScreen.Destination> dest_list){
+        this.dest_list = dest_list;
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView destName;
+        ImageView destPhoto;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv);
+            destName = (TextView)itemView.findViewById(R.id.dest_name);
+            destPhoto = (ImageView)itemView.findViewById(R.id.dest_photo);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.destination_card, viewGroup, false);
+        ViewHolder pvh = new ViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder ViewHolder, int i) {
+        ViewHolder.destName.setText(dest_list.get(i).name);
+        ViewHolder.destPhoto.setImageResource(dest_list.get(i).photoId);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dest_list.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+
+}
