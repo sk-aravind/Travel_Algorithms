e1a73e463788d368ac4f128277b908d06b7fa5a8
sk-aravinda <sk.aravind.93@gmail.com>
2016-11-10 02:57:28 +0800

    Base Structure,UI elements and Object Classes

--- app/src/main/java/com/skara/triggered/travelapp_triggered/HomeScreen.java
+++ app/src/main/java/com/skara/triggered/travelapp_triggered/HomeScreen.java
@@ -1,14 +1,9 @@
 package com.skara.triggered.travelapp_triggered;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
@@ -29,7 +24,7 @@ import static com.skara.triggered.travelapp_triggered.R.layout.destination_card;
 
 public class HomeScreen extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,97 +32,72 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
+        RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
         initializeData();
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv.setLayoutManager(llm);
         RVAdapter adapter = new RVAdapter(dest_list);
         rv.setAdapter(adapter);
 
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-
-        ActionBar supportActionBar = getSupportActionBar();
-        if (supportActionBar != null) {
-            supportActionBar.setHomeAsUpIndicator(R.drawable.ic_reorder_black_24dp);
-            supportActionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        menuItem.setChecked(true);
-                        mDrawerLayout.closeDrawers();
-                        return true;
-                    }
-                });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
     }
 
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
 
+        return super.onOptionsItemSelected(item);
+    }
+// Data ===============================================================================
+    class Destination {
+        String name;
 
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
+        int photoId;
 
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
-            else if (id == android.R.id.home) {
-                mDrawerLayout.openDrawer(GravityCompat.START);
-            }
-
-            return super.onOptionsItemSelected(item);
+        Destination(String name,  int photoId) {
+            this.name = name;
+            this.photoId = photoId;
         }
-
-    public void openRouteOptions(View view) {
-        Intent intent = new Intent(this, Route_options.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 
+    private List<Destination> dest_list;
+    // dest_list = destination list
 
 
-    public
-// Data ===============================================================================
-        class Destination {
-            String name;
-
-            int photoId;
-
-            Destination(String name, int photoId) {
-                this.name = name;
-                this.photoId = photoId;
-            }
-        }
-
-        private List<Destination> dest_list;
-        // dest_list = destination list
-
-
-    private void initializeData() {
+    private void initializeData(){
         dest_list = new ArrayList<>();
         dest_list.add(new Destination("THE ZOO", R.drawable.a));
-        dest_list.add(new Destination("MARINA BAY", R.drawable.b));
-        dest_list.add(new Destination("SUTD", R.drawable.c));
-        dest_list.add(new Destination("THE ZOO", R.drawable.a));
-        dest_list.add(new Destination("MARINA BAY", R.drawable.b));
+        dest_list.add(new Destination("MARINA BAY",R.drawable.b));
         dest_list.add(new Destination("SUTD", R.drawable.c));
 
     }
@@ -135,5 +105,4 @@ public class HomeScreen extends AppCompatActivity {
 
 
 }
-
 
