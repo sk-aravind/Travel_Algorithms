d1353e78f16cff02c86aff9c5aa2deb820e5e708
kirinzloh <weiquan7@hotmail.com>
2016-11-16 18:23:15 +0800

    wei

--- app/src/main/java/com/skara/triggered/travelapp_triggered/RVAdapterForTR.java
+++ app/src/main/java/com/skara/triggered/travelapp_triggered/RVAdapterForTR.java
@@ -10,18 +10,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Skara on 9/11/16.
  */
 
-public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
-//all the methods needs to be overwritten to prevent error
+public class RVAdapterForTR extends RecyclerView.Adapter<RVAdapterForTR.ViewHolder> {
+    //all the methods needs to be overwritten to prevent error
     List<HomeScreen.Destination> dest_list;
 
-    RVAdapter(List<HomeScreen.Destination> dest_list){
+    RVAdapterForTR(List<HomeScreen.Destination> dest_list){
         this.dest_list = dest_list;
     }
 
@@ -30,29 +29,20 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
         CardView cv;
         TextView destName;
         ImageView destPhoto;
-        FloatingActionButton addBtn;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
             cv = (CardView)itemView.findViewById(R.id.cv);
             destName = (TextView)itemView.findViewById(R.id.dest_name);
             destPhoto = (ImageView)itemView.findViewById(R.id.dest_photo);
-            addBtn = (FloatingActionButton)itemView.findViewById(R.id.addBtn);
 
-            addBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String dest = destName.getText().toString();
-                    Toast.makeText(view.getContext(),"Added " + dest, Toast.LENGTH_SHORT).show();
-                    addToItinerary(destName.getText().toString(),(Integer) destPhoto.getTag());
-                }
-            });
         }
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.destination_card, viewGroup, false);
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.travel_route_card, viewGroup, false);
         ViewHolder pvh = new ViewHolder(v);
         return pvh;
     }
@@ -74,16 +64,4 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
         super.onAttachedToRecyclerView(recyclerView);
     }
 
-
-    public void addToItinerary(String s,int i){
-        if (HomeScreen.iti_list == null){
-            HomeScreen.iti_list = new ArrayList<>();
-        }
-        for (HomeScreen.Destination j : HomeScreen.iti_list){
-            if (j.name==s){
-                return;
-            }
-        }
-        HomeScreen.iti_list.add(new HomeScreen.Destination(s, i));
-    }
 }
