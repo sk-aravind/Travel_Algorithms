c74b989a701d1ebdbfab20e01f5cf8c98b157dbb
kirinzloh <weiquan7@hotmail.com>
2016-11-17 18:05:14 +0800

    Fix transition to details and add btn

---
+++ app/src/main/java/com/skara/triggered/travelapp_triggered/DetailsInterface.java
@@ -0,0 +1,7 @@
+package com.skara.triggered.travelapp_triggered;
+
+import android.view.View;
+
+public interface DetailsInterface {
+    void showDestDetails(View view, String destName, Integer destPhoto);
+}
