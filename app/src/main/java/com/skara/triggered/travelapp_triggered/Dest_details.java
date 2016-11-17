bb29b85c896b6ec6032d4185c0cf89f598b7c3f4
Skara <sk.aravind.93@gmail.com>
2016-11-17 19:16:07 +0800

    Maps Done

--- app/src/main/java/com/skara/triggered/travelapp_triggered/Dest_details.java
+++ app/src/main/java/com/skara/triggered/travelapp_triggered/Dest_details.java
@@ -1,12 +1,14 @@
 package com.skara.triggered.travelapp_triggered;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,6 +33,15 @@ public class Dest_details extends AppCompatActivity {
 
         textView.setText(name);
         imageView.setImageResource(img);
+
+        final Button button = (Button) findViewById(R.id.button_link);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.google.com"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
     }
 //
--- app/src/main/java/com/skara/triggered/travelapp_triggered/MapsActivity.java
+++ app/src/main/java/com/skara/triggered/travelapp_triggered/MapsActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
@@ -16,7 +17,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import static java.security.AccessController.getContext;
@@ -29,17 +33,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     String attraction;
 
     GoogleMap map;
-
+    LatLng fromadd;
 
     boolean plot_straight_route = false;
     AutoCompleteTextView acTextView;
     ArrayAdapter<String> adapter;
     View v;
+    int BUILDING_LEVEL = 20;
+    Place currentPlace = null;
+    int STREET_LEVEL = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -48,42 +56,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
+        autocompleteFragment.setBoundsBias(new LatLngBounds(
+                new LatLng(1.194257, 103.551421),
+                new LatLng(1.547341, 104.073343)));
+
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
                 // TODO: Get info about the selected place.
+                Toast.makeText(MapsActivity.this, "Place: " + place.getAddress(), Toast.LENGTH_LONG).show();
+                currentPlace = place;
+                mMap.addMarker(new MarkerOptions().position(place.getLatLng()));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), STREET_LEVEL));
 
             }
 
             @Override
             public void onError(Status status) {
                 // TODO: Handle the error.
-
+                Toast.makeText(getBaseContext(), "failure", Toast.LENGTH_LONG).show();
             }
         });
-
-
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-//                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
-//        AutoCompleteTextView textView = (AutoCompleteTextView)
-//                findViewById(R.id.countries_list);
-//        textView.setAdapter(adapter);
-
-//        Database db = new Database();
-//        adapter = new ArrayAdapter<>(getContext(),android.R.layout.select_dialog_singlechoice,db.getList());
-//        acTextView= (AutoCompleteTextView)v.findViewById(R.id.autocomplete_attraction_input);
-//        acTextView.setThreshold(1);
-//        acTextView.setAdapter(adapter);
-//
-//
-//        Button button_search = (Button) v.findViewById(R.id.search);
-//        button_search.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View view) {
-////                search(view);
-//            }
-//        });
 
     }
 
@@ -101,13 +94,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+        //Set the initial spot to edmonton for now
+        LatLng singapore = new LatLng(1.370606, 103.804709);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(singapore)      // Sets the center of the map to location user
+                .zoom(10)
+                .bearing(0)
+                .tilt(0)
+                .build();
 
-    private static final String[] COUNTRIES = new String[] {
-            "Belgium", "France", "Italy", "Germany", "Spain"
-    };
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(13));
+
+    }
 }
